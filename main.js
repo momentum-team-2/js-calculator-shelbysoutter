6b256a93ac2360e3689469d26920cfc23bb0f1d9
shelby_soutter <ssa.soutter@gmail.com>
2020-05-28 19:26:38 -0400

    Buttons return number when clicked.

---
+++ main.js
@@ -0,0 +1,70 @@
+//event listeners for button clicks
+let button_7 = document.querySelector("#seven")
+button_7.addEventListener("click", function(event) {
+    console.log (7)
+})
+
+let button_8 = document.querySelector("#eight")
+button_8.addEventListener("click", function(event) {
+    console.log (8)
+})
+
+let button_9 = document.querySelector("#nine")
+button_9.addEventListener("click", function(event) {
+    console.log (9)
+})
+
+let button_slash = document.querySelector("#slash")
+button_slash.addEventListener("click", function(event) {
+    console.log ("/")
+})
+
+let button_4 = document.querySelector("#four")
+button_4.addEventListener("click", function(event) {
+    console.log (4)
+})
+
+let button_5 = document.querySelector("#five")
+button_5.addEventListener("click", function(event) {
+    console.log (5)
+})
+
+let button_6 = document.querySelector("#six")
+button_6.addEventListener("click", function(event) {
+    console.log (6)
+})
+
+let button_0 = document.querySelector("#zero")
+button_0.addEventListener("click", function(event) {
+    console.log (0)
+})
+
+let button_1 = document.querySelector("#one")
+button_1.addEventListener("click", function(event) {
+    console.log (1)
+})
+
+let button_2 = document.querySelector("#two")
+button_2.addEventListener("click", function(event) {
+    console.log (2)
+})
+
+let button_3 = document.querySelector("#three")
+button_3.addEventListener("click", function(event) {
+    console.log (3)
+})
+
+let button_decimal = document.querySelector("#decimal")
+button_decimal.addEventListener("click", function(event) {
+    console.log (".")
+})
+
+let button_equal = document.querySelector("#equal")
+button_equal.addEventListener("click", function(event) {
+    console.log ("=")
+})
+
+let button_plus = document.querySelector("#plus")
+button_plus.addEventListener("click", function(event) {
+    console.log ("+")
+})
